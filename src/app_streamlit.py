176fe6758d762d8d70db2e8d22af78bca73e74a6
Abdullah Fahlo <abdullahfahlo.com@gmail.com>
2025-10-06 17:57:18 +0300

    Update app_streamlit.py

--- src/app_streamlit.py
+++ src/app_streamlit.py
@@ -3,12 +3,7 @@ import json
 import os
 #from mcq_pipeline_final_v2 import generate_mcqs_from_text
 import spacy
-from spacy.cli import download
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 
 # ----------------------------
